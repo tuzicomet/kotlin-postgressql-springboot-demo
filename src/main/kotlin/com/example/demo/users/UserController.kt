357a7a059f4ba0ae2fd7ec310e8949dcd04e8652
tuzicomet <leo336570@gmail.com>
2025-07-29 19:19:44 +1200

    Set up UserController with basic route and injected repository
    Created a REST controller mapped to /api/users and injected UserRepository using constructor injection (@Autowired). Endpoint methods still to be added.

---
+++ src/main/kotlin/com/example/demo/users/UserController.kt
@@ -0,0 +1,12 @@
+package com.example.demo.users
+
+import org.springframework.data.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/users")
+class UserController(@Autowired private val userRepository: UserRepository){
+
+}
