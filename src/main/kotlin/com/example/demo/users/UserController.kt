02629c4a69a1750f6b7e996923f4c9baa7ce2f38
tuzicomet <leo336570@gmail.com>
2025-07-29 20:24:53 +1200

    fixed incorrect import for spring beans in UserController

--- src/main/kotlin/com/example/demo/users/UserController.kt
+++ src/main/kotlin/com/example/demo/users/UserController.kt
@@ -1,6 +1,6 @@
 package com.example.demo.users
 
-import org.springframework.data.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
